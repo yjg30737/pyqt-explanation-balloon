2e0be8d55cc5d92f1a1b593879b70795aa47d1ba
Jung Gyu Yoon <yjg30737@gmail.com>
2022-06-13 10:37:40 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-explanation-balloon',
@@ -10,6 +18,8 @@ setup(
     package_data={'pyqt_explanation_balloon.ico': ['close.svg']},
     description='PyQt explanation balloon',
     url='https://github.com/yjg30737/pyqt-explanation-balloon.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
         'pyqt-svg-button>=0.0.1'
